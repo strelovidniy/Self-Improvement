4e24b141719995469fe2b9ac8288c94dc153851e
Remok777 <roman.sadokha01@gmail.com>
2021-11-15 21:09:58 +0200

    Added initial bot markup

---
+++ Self.Improvement.Domain/Bot/SelfImprovementBot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace Bot
+{
+    class SelfImprovementBot
+    {
+        static void Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("2137745577:AAE4XONfwYQ9gLwDQbp_Y9jSeJoylLj8-fw");
+            var me = botClient.GetMeAsync();
+            Console.WriteLine(
+                $"Hello, World! I am user {me.Id}"
+            );
+        }
+    }
+}
