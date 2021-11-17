acb65ad1f44dce4e3310e8eac273588c30faffa9
Reverend <roma.dan2001@gmail.com>
2021-11-18 00:49:27 +0200

    test tg bot

--- Self.Improvement.Web/Startup.cs
+++ Self.Improvement.Web/Startup.cs
@@ -19,7 +19,7 @@ namespace Self.Improvement.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllers().AddNewtonsoftJson();
 
             services.AddServices();
 
