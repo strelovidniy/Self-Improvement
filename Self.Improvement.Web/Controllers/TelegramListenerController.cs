063eae5484a4335820edb8d525c37d4de03183aa
Reverend <roma.dan2001@gmail.com>
2021-11-18 00:34:22 +0200

    Test bot

--- Self.Improvement.Web/Controllers/TelegramListenerController.cs
+++ Self.Improvement.Web/Controllers/TelegramListenerController.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Self.Improvement.Data.Enums;
 using Self.Improvement.Domain.Configs;
 using Self.Improvement.Domain.Services.Interfaces;
 using Self.Improvement.Domain.TelegramBot;
 using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
+using Message = Self.Improvement.Data.Entities.Message;
 
 namespace Self.Improvement.Web.Controllers
 {
@@ -17,20 +20,38 @@ namespace Self.Improvement.Web.Controllers
         private readonly ChatBot _tgBot;
         private readonly IOptions<ChatBotConfig> _accesToken;
         private readonly ITelegramHandlersService _tgHandler;
+        private readonly IChatService _chatService;
 
-        public TelegramListenerController(ChatBot tgBot, IOptions<ChatBotConfig> accesToken, ITelegramHandlersService tgHandler)
+        public TelegramListenerController(
+            ChatBot tgBot,
+            IOptions<ChatBotConfig> accesToken,
+            ITelegramHandlersService tgHandler,
+            IChatService chatService
+        )
         {
             _tgBot = tgBot;
             _accesToken = accesToken;
             _tgHandler = tgHandler;
+            _chatService = chatService;
         }
-        
+
         [HttpPost("update")]
         public async Task<IActionResult> Update([FromBody] Update update, CancellationToken cancellationToken)
         {
             _tgBot.Init(_accesToken.Value.AccessToken);
             _tgBot.Start(_tgHandler.HandleUpdateAsync, _tgHandler.HandleErrorAsync);
             await _tgBot.Client.SendTextMessageAsync(update.Message.Chat.Id, "Hello");
+            _chatService.SendMessageAsync(new Message
+            {
+                ChatId = new Guid("146fc3d8-22c6-40dd-a052-2a7b853739bd"),
+                Date = DateTime.Now,
+                FromBot = true,
+                Id = new Guid(),
+                Status = MessageStatus.Unread,
+                TelegramChatId = (int)update.Message.Chat.Id,
+                Text = "Bot Works",
+            });
+
             return Ok(update.Message.Text);
         }
     }
